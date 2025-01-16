cb0e99b7c7f45e08ac519c60e7fb1be6e6d490b8
Kjw072892 <kassie.j.whitney92@gmail.com>
2025-01-15 17:01:07 -0800

    Completed StoreItem Class

--- src/edu/uw/tcss/model/StoreItem.java
+++ src/edu/uw/tcss/model/StoreItem.java
@@ -2,39 +2,122 @@ package edu.uw.tcss.model;
 
 import java.math.BigDecimal;
 
+/**
+ * @author Kassie Whitney
+ * @version 1.15.25
+ */
 public class StoreItem implements Item {
 
-    public StoreItem(final String name, final BigDecimal price) {
+    /**
+     * Field Variable that stores the item name.
+     */
+    private final String myName;
+
+    /**
+     * Stores the item price
+     */
+    private final BigDecimal myPrice;
+
+    /**
+     * Stores the item bulk quantity
+     */
+    private final int myBq;
+
+    /**
+     * Stores the item's price in bulk
+     */
+    private final BigDecimal myBp;
+
+    /**
+     * Stores boolean if item is bulk
+     */
+    private final boolean myIsBulk;
 
 
+    /**
+     *
+     * Constructor for the StoreItem class.
+     *
+     * @param theName The name of the item.
+     * @param thePrice  The price of the item.
+     */
+    public StoreItem(final String theName, final BigDecimal thePrice)
+            throws IllegalArgumentException, NullPointerException {
+
+        super();
+
+        if (theName.isEmpty() || thePrice == null) {
+
+            throw new NullPointerException("Parameters must not be null");
+        }
+        if (thePrice.intValue() < 0) {
+
+            throw new IllegalArgumentException("Price must be greater than or equal to zero");
+
+        } else {
+
+            myName = theName;
+            myPrice = thePrice;
+            myBq = 0;
+            myBp = BigDecimal.ZERO;
+            myIsBulk = false;
+
+        }
     }
 
-    public StoreItem(final String name, final BigDecimal price, final int bq,
-                     final BigDecimal bp) {
+    /**
+     *
+     * Constructor for the StoreItem Class.
+     *
+     * @param theName The name of the item.
+     * @param thePrice The price of the item.
+     * @param theBq The bulk quanity of the item.
+     * @param theBp The price of the item in bulk quanity.
+     */
+    public StoreItem(final String theName, final BigDecimal thePrice, final int theBq,
+                     final BigDecimal theBp) throws IllegalArgumentException {
+
+        super();
+
+        new StoreItem(theName, thePrice);
+
+        if (0 <= theBq && 0.0 <= theBp.doubleValue()) {
+
+            myName = theName;
+            myPrice = thePrice;
+            myBq = theBq;
+            myBp = theBp;
+            myIsBulk = true;
+
+        } else {
+
+            throw new IllegalArgumentException("Parameters must be greater than zero");
+
+        }
     }
 
     @Override
     public String getName() {
-        return null;
+        return myName;
     }
 
     @Override
     public BigDecimal getPrice() {
-        return null;
+        return myPrice;
     }
 
     @Override
     public int getBulkQuantity() {
-        return 0;
+        return myBq;
     }
 
     @Override
     public BigDecimal getBulkPrice() {
-        return null;
+        return myBp;
     }
 
     @Override
     public boolean isBulk() {
-        return false;
+        return myIsBulk;
     }
 }
